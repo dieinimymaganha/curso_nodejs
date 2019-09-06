ca8f49e9fc2a472f82655ed31577c367db1aa991
Dieinimy <dieinimy@gmail.com>
2019-09-06 17:20:18 -0300

    criando modulo fatorial

---
+++ node-examples/fatorial.js
@@ -0,0 +1,13 @@
+const fatorial = (num)=>{
+    if(num === 0) {
+        return 1
+    }
+    return num * fatorial (num - 1)
+}
+
+// exports.fatorial = fatorial
+
+// exports = module.exports
+
+module.exports = fatorial
+
