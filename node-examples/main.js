db087ecfe8d2a49bbacbeb04b227456270b95b07
Dieinimy <dieinimy@gmail.com>
2019-09-06 17:38:50 -0300

    Importando o modulo yargs e fazendo as alterações para utilização

--- node-examples/main.js
+++ node-examples/main.js
@@ -2,13 +2,8 @@ const fatorial = require('./fatorial').fatorial
 
 console.log('n-fatorial')
 
+const argv = require('yargs').demandOption('num').argv
 
-/*console.log('Executando o script a partir do diretório ${process.cwd()}')
-
-process.on('exit', () =>{
-  console.log('Script está proximo a terminar')
-})*/
-
-const num = parseInt(process.argv[2])
+const num = argv.num
 
 console.log("O fatorial de ${num} é igual a ${fatorial(num)}")
--- node-examples/save-file.js
+++ node-examples/save-file.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
+const argv = require('yargs')
+    .alias('f', 'filename')
+    .alias('c', 'content')
+    .demandOption('filename')
+    .demandOption('content').argv
 
-fs.writeFile(process.argv[2], process.argv[3], (error)=>{
+fs.writeFile(argv.filename, argv.content, (error)=>{
     if(error) throw error
-    console.log('Arquivo ${process.arv[2]}, foi salvo com sucesso')
+    console.log('Arquivo ${arv.filename}, foi salvo com sucesso')
 })
