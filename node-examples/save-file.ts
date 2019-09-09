9ab4f28a59c6e39814e10055d5530d63fac00444
Dieinimy <dieinimy@gmail.com>
2019-09-09 11:39:54 -0300

    Deletando arquivos e criando novos arquivos ts

--- node-examples/save-file.ts
+++ node-examples/save-file.ts
@@ -2,12 +2,13 @@ import * as fs from 'fs'
 import * as yargs from 'yargs'
 
 const argv = yargs
-    .alias('f', 'filename')
-    .alias('c', 'content')
-    .demandOption('filename')
-    .demandOption('content').argv
+      .alias('f', 'filename')
+      .alias('c', 'content')
+      .demandOption('filename')
+      .demandOption('content')
+      .argv
 
 fs.writeFile(argv.filename, argv.content, (error)=>{
     if(error) throw error
-    console.log(`Arquivo ${argv.filename}, foi salvo com sucesso`)
+    console.log(`Arquivo ${argv.filename} foi salvo com sucesso.`)
 })
