281b40bd850bea546a58412423c13090c6073e29
Dieinimy <dieinimy@gmail.com>
2019-09-09 10:36:14 -0300

    Incluido module.paths para mostrar os diretórios que o node busca um modulo ao passar o require

--- node-examples/main.js
+++ node-examples/main.js
@@ -7,3 +7,8 @@ const argv = require('yargs').demandOption('num').argv
 const num = argv.num
 
 console.log(`O fatorial de ${num} é igual a ${fatorial(num)}`)
+
+/*Exibi os caminhos que o node busca para encontrar
+um modulo quando é passado um require
+*/
+console.log(module.paths)
