77d247127b88e5ca57282a20251861ae6c20dbd4
Dieinimy <dieinimy@gmail.com>
2019-09-09 14:55:54 -0300

    Usando o next para validar o browser e dar mensagem caso esteja em um determinado browser

--- meat-api/main.ts
+++ meat-api/main.ts
@@ -5,10 +5,32 @@ const server = restify.createServer({
   version: '1.0.0'
 })
 
-server.get('/hello', (req, resp, next)=>{
-  resp.json({message: 'hello'})
+server.use(restify.plugins.queryParser())
+
+server.get('/info',[
+  (req, resp, next) =>{
+    if(req.userAgent() && req.userAgent().includes('MSIE 7.0')){
+      resp.status(400)
+      resp.json({message: 'Please, update your browser'})
+      return next(false)
+    }
+    return next()
+  },
+  (req, resp, next)=>{
+  // resp.contentType = 'application/json'
+  // resp.status(400)
+  // resp.setHeader('Content-Type', 'application/json')
+  // resp.send({message: 'hello'})
+  resp.json({
+    browser: req.userAgent(),
+    method: req.method,
+    url: req.href(),
+    path: req.path(),
+    query: req.query
+
+  })
   return next()
-})
+}])
 
 server.listen(3000, ()=>{
   console.log('Api is running on http://localhost:3000')
