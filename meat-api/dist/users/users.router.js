71c4e9b1c883c0df4b4df0d4e23a85eaedfb9e71
Dieinimy <dieinimy@gmail.com>
2019-09-09 17:02:08 -0300

    Incluido parse users.router

--- meat-api/dist/users/users.router.js
+++ meat-api/dist/users/users.router.js
@@ -1,10 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const router_1 = require("../common/router");
+const users_model_1 = require("./users.model");
 class UsersRouter extends router_1.Router {
     applyRouters(application) {
         application.get('/users', (req, resp, next) => {
-            resp.json({ messagem: 'users router' });
+            users_model_1.User.findAll().then(users => {
+                resp.json(users);
+                return next();
+            });
         });
     }
 }
