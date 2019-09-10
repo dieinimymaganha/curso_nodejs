9930b98021e3fbb4bdf66ea02d7932bb9c69182e
Dieinimy <dieinimy@gmail.com>
2019-09-10 16:57:18 -0300

    Realizando a refatoração incluindo a função render

--- meat-api/users/users.router.ts
+++ meat-api/users/users.router.ts
@@ -6,32 +6,17 @@ class UsersRouter extends Router{
   applyRouters(application: restify.Server){
 
     application.get('/users', (req, resp, next)=>{
-      User.find().then(users=>{
-        resp.json(users)
-        return next()
-      })
+      User.find().then(this.render(resp, next))
     })
 
     application.get('/users/:id', (req, resp, next)=>{
-      User.findById(req.params.id).then(user=>{
-        if(user){
-          resp.json(user)
-          return next()
-        }
-
-        resp.send(404)
-        return next()
-
-      })
+      User.findById(req.params.id)
+          .then(this.render(resp, next))
     })
 
     application.post('/users', (req, resp, next)=>{
       let user = new User(req.body)
-      user.save().then(user=>{
-        user.password = undefined
-        resp.json(user)
-        return next()
-      })
+      user.save().then(this.render(resp, next))
     })
 
     application.put('/users/:id', (req, resp, next)=>{
@@ -43,22 +28,13 @@ class UsersRouter extends Router{
             }else{
               resp.send(404)
             }
-      }).then(user =>{
-        resp.json(user)
-        return next()
-      })
+      }).then(this.render(resp, next))
     })
 
     application.patch('/users/:id', (req, resp, next)=>{
       const options = {new : true}
-      User.findByIdAndUpdate(req.params.id, req.body, options).then(user =>{
-        if(user){
-          resp.json(user)
-          return next()
-        }
-        resp.send(404)
-        return next()
-      })
+      User.findByIdAndUpdate(req.params.id, req.body, options)
+          .then(this.render(resp, next))
     })
 
     application.del('/users/:id', (req, resp, next) =>{
