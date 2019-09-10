561b761c585f85577b6841b29b38dd937f9724e0
Dieinimy <dieinimy@gmail.com>
2019-09-10 11:48:28 -0300

    Incluindo rota user com o metodo post

--- meat-api/dist/users/users.router.js
+++ meat-api/dist/users/users.router.js
@@ -21,10 +21,11 @@ class UsersRouter extends router_1.Router {
             });
         });
         application.post('/users', (req, resp, next) => {
-            let user = new users_model_1.User();
-            user.name = 'Diana';
-            user.email = 'email';
-            user.save();
+            let user = new users_model_1.User(req.body);
+            user.save().then(user => {
+                resp.json(user);
+                return next();
+            });
         });
     }
 }
--- meat-api/users/users.router.ts
+++ meat-api/users/users.router.ts
@@ -25,6 +25,14 @@ class UsersRouter extends Router{
       })
     })
 
+    application.post('/users', (req, resp, next)=>{
+      let user = new User(req.body)
+      user.save().then(user=>{
+        resp.json(user)
+        return next()
+      })
+    })
+
   }
 }
 
