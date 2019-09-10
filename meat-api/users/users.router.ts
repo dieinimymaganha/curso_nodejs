1681ba8165b94e7d3f4d1657992aa5501d7eda5e
Dieinimy <dieinimy@gmail.com>
2019-09-10 17:22:05 -0300

    Incluindo o Catch para começarmos a tratar os erros

--- meat-api/users/users.router.ts
+++ meat-api/users/users.router.ts
@@ -14,17 +14,22 @@ class UsersRouter extends Router{
   applyRouters(application: restify.Server){
 
     application.get('/users', (req, resp, next)=>{
-      User.find().then(this.render(resp, next))
+      User.find()
+          .then(this.render(resp, next))
+          .catch(next)
     })
 
     application.get('/users/:id', (req, resp, next)=>{
       User.findById(req.params.id)
           .then(this.render(resp, next))
+          .catch(next)
     })
 
     application.post('/users', (req, resp, next)=>{
       let user = new User(req.body)
-      user.save().then(this.render(resp, next))
+      user.save()
+          .then(this.render(resp, next))
+          .catch(next)
     })
 
     application.put('/users/:id', (req, resp, next)=>{
@@ -37,12 +42,14 @@ class UsersRouter extends Router{
               resp.send(404)
             }
       }).then(this.render(resp, next))
+        .catch(next)
     })
 
     application.patch('/users/:id', (req, resp, next)=>{
       const options = {new : true}
       User.findByIdAndUpdate(req.params.id, req.body, options)
           .then(this.render(resp, next))
+          .catch(next)
     })
 
     application.del('/users/:id', (req, resp, next) =>{
@@ -53,7 +60,7 @@ class UsersRouter extends Router{
           resp.send(404)
         }
         return next()
-      })
+      }).catch(next)
     })
   }
 }
