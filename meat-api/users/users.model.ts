0fc825a50842389a2f295d8900a235125012aacc
Dieinimy <dieinimy@gmail.com>
2019-09-11 09:48:09 -0300

    Incluindo expressão regular para validação do e-mail

--- meat-api/users/users.model.ts
+++ meat-api/users/users.model.ts
@@ -15,7 +15,8 @@ const userSchema = new mongoose.Schema({
   },
   email:{
     type: String,
-    unique: true,
+    unique: true, 
+    match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     required: true
   },
   password: {
