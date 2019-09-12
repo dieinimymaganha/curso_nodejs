b7ed900add60a4432c5d8f760aa498b036b80b41
Dieinimy <dieinimy@gmail.com>
2019-09-12 16:48:49 -0300

    Criando a model de reviews

---
+++ meat-api/reviews/reviews.model.ts
@@ -0,0 +1,41 @@
+import * as mongoose from 'mongoose'
+import {Restaurant} from '../restaurants/restaurants.model'
+import {User} from '../users/users.model'
+
+
+export interface Review extends mongoose.Document {
+  date: Date,
+  rating: number,
+  comments: string,
+  restaurant: mongoose.Types.ObjectId | Restaurant,
+  user: mongoose.Types.ObjectId | User
+}
+
+const reviwSchema = new mongoose.Schema({
+  data:{
+    type: Date,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
+  comments: {
+    type: String,
+    required: true,
+    maxlength: 500
+  },
+  restaurant:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Restaurant',
+    required: true
+  },
+  user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+})
+
+
+const Review = mongoose.model<Review>('Review', reviwSchema)
