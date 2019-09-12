6a4a2b217266d2f2c3c9c994bab6a7cc625bf556
Dieinimy <dieinimy@gmail.com>
2019-09-12 13:37:55 -0300

    Criado a rota de restaurant

---
+++ meat-api/restaurants/restaurants.router.ts
@@ -0,0 +1,20 @@
+import {ModelRouter} from '../common/model-router'
+import * as restify from 'restify'
+import {NotFoundError} from 'restify-errors'
+import {Restaurant} from './restaurants.model'
+
+class RestaurantsRouter extends ModelRouter<Restaurant> {
+  constructor(){
+    super(Restaurant)
+  }
+
+    applyRouters(application: restify.Server){
+      application.get('/restaurants', this.findAll)
+      application.get('/restaurants/:id', [this.validateId, this.findById])
+      application.post('/restaurants', this.save)
+      application.put('/restaurants/:id', [this.validateId, this.replace])
+      application.patch('/restaurants/:id', [this.validateId, this.update])
+      application.del('/restaurants/:id',[this.validateId, this.delete])
+    }
+
+}
