21d081e5c8c8b151f22eea80afa4b73af04ac366
Dieinimy <dieinimy@gmail.com>
2019-09-16 17:11:53 -0300

    Criando banco de dados para teste e criando função para matar o processo de test

--- meat-api/users/users.test.ts
+++ meat-api/users/users.test.ts
@@ -1,17 +1,26 @@
 import 'jest'
 import * as request from 'supertest'
+import {Server} from '../server/server'
+import {enviroment}  from '../common/enviroment'
+import {usersRouter} from './users.router'
+import {User} from './users.model'
 
-test('get /users', () =>{
-  return request('http://localhost:3000')
-    .get('/users')
-    .then(response=>{
-      expect(response.status).toBe(200)
-      expect(response.body.items).toBeInstanceOf(Array)
-    }).catch(fail)
+let address: string
+let server: Server
+
+beforeAll(() => {
+  enviroment.db.url = process.env.DB_URL || 'mongodb://localhost/meat-api-test-db'
+  enviroment.server.port = process.env.SERVER_PORT || 3001
+  address = `http://localhost:${enviroment.server.port}`
+  server = new Server()
+  return server.bootstrap([usersRouter])
+               .then(() => User.remove({}).exec())
+               .catch(console.error)
 })
 
+
 test('post / users', () => {
-  return request('http://localhost:3000')
+  return request(address)
   .post('/users')
   .send({
     name: 'usuario1',
@@ -27,4 +36,19 @@ test('post / users', () => {
     expect(response.body.cpf).toBe('962.116.531-82')
     expect(response.body.password).toBeUndefined()
   }).catch(fail)
+})
+
+
+
+test('get /users', () =>{
+  return request(address)
+    .get('/users')
+    .then(response=>{
+      expect(response.status).toBe(200)
+      expect(response.body.items).toBeInstanceOf(Array)
+    }).catch(fail)
+})
+
+afterAll(() =>{
+  return server.shutdown()
 })
