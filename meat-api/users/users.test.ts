012f544bb2e64481ffcd9f086486d28c3bd53d1f
Dieinimy <dieinimy@gmail.com>
2019-09-16 16:17:54 -0300

    Criando testes post para users

--- meat-api/users/users.test.ts
+++ meat-api/users/users.test.ts
@@ -9,3 +9,22 @@ test('get /users', () =>{
       expect(response.body.items).toBeInstanceOf(Array)
     }).catch(fail)
 })
+
+test('post / users', () => {
+  return request('http://localhost:3000')
+  .post('/users')
+  .send({
+    name: 'usuario1',
+    email: 'usuario@email.com',
+    password: '123456',
+    cpf: '962.116.531-82'
+  })
+  .then(response=>{
+    expect(response.status).toBe(200)
+    expect(response.body._id).toBeDefined()
+    expect(response.body.name).toBe('usuario1')
+    expect(response.body.email).toBe('usuario@email.com')
+    expect(response.body.cpf).toBe('962.116.531-82')
+    expect(response.body.password).toBeUndefined()
+  }).catch(fail)
+})
