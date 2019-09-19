9b38a8d5f30cba2ee31817babe942bb944599085
Dieinimy <dieinimy@gmail.com>
2019-09-19 17:40:17 -0300

    Terminando a segunda parte da criação do token

--- meat-api/security/auth.handler.ts
+++ meat-api/security/auth.handler.ts
@@ -1,6 +1,8 @@
 import * as restify from 'restify'
+import * as jwt from 'jsonwebtoken'
 import {NotAuthorizedError} from 'restify-errors'
 import {User} from '../users/users.model'
+import {enviroment} from '../common/enviroment'
 
 
 export const authenticate: restify.RequestHandler = (req, resp, next) =>{
@@ -10,6 +12,10 @@ export const authenticate: restify.RequestHandler = (req, resp, next) =>{
         if(user && user.matches(password)){ // 2nd
           //gerar o token
           // 3rd
+          const token = jwt.sign({sub: user.email, iss: 'meat-api'},
+          enviroment.security.apiSecret)
+          resp.json({name: user.name, email: user.email, acessToken: token})
+          return next(false)
         }else{
           return next(new NotAuthorizedError('Invalid Credentials'))
 
