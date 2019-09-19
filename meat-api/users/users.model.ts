4bc8c2ff17705b22108e1a016ecd81b802c5607a
Dieinimy <dieinimy@gmail.com>
2019-09-19 16:54:51 -0300

    Iniciando a criação do token

--- meat-api/users/users.model.ts
+++ meat-api/users/users.model.ts
@@ -6,11 +6,14 @@ import {enviroment} from '../common/enviroment'
 export interface User extends mongoose.Document {
   name: String,
   email: String,
-  password: String
+  password: String,
+  cpf: String,
+  gender: String,
+  matches(password: string) : boolean
 }
 
 export interface  UserModel extends mongoose.Model<User>{
-  findByEmail(email: string) : Promise<User>
+  findByEmail(email: string, projection?: string) : Promise<User>
 }
 
 const userSchema = new mongoose.Schema({
@@ -47,8 +50,12 @@ const userSchema = new mongoose.Schema({
 
 })
 
-userSchema.statics.findByEmail = function(email: string){
-    return this.findOne({email}) // {email: email}
+userSchema.statics.findByEmail = function(email: string, projection: string){
+    return this.findOne({email}, projection) // {email: email}
+}
+
+userSchema.methods.matches = function(password: string): boolean {
+  return bcrypt.compareSync(password, this.password)
 }
 
 const hashPassword = (obj, next)=>{
